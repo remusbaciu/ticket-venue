418ddea443830ba72566c643e780a4232b9f9dfb
remus.baciu <remus.baciu>
2019-04-12 12:17:07 +0100

    Added validation around the user password

--- src/main/java/actions/User.java
+++ src/main/java/actions/User.java
@@ -22,7 +22,15 @@ public abstract class User {
         this.username = username;
         System.out.println("Enter password:");
         password = cin.next();
+
+        while(password.length() > 10 || password.length() < 4){
+            System.out.println("Your password's length should be between 4 and 10 characters.");
+            System.out.println("Please re-enter your password:");
+            password = cin.next();
+        }
+
         this.password = password;
         System.out.println("You have successfully signed in as " + username);
     }
 }
+
